5e5ce5510d6180a9280c98d957ee05f1e36b24dc
diegowholikesorange <dieg@tognola.net>
2017-05-15 14:55:33 +1000

    adding a button

--- src/RefreshButton.js
+++ src/RefreshButton.js
@@ -1,3 +1,24 @@
-/**
- * Created by diego on 15/05/17.
- */
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { Button } from 'react-buttons';
+
+
+
+class RefreshButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <Button faIcon="plus"  >New Thing</Button>
+            </div>
+        )
+    }
+}
+
+export default RefreshButton;
+
