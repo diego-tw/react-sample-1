95847e6ccc5388c22fa633c5b7e2af26869c3cf1
diegowholikesorange <dieg@tognola.net>
2017-05-15 15:39:27 +1000

    adding primitive list

--- src/components/KeysList.js
+++ src/components/KeysList.js
@@ -1,3 +1,24 @@
-/**
- * Created by diego on 15/05/17.
- */
+import React, {Component} from 'react'
+import ReactScrollableList from 'react-scrollable-list'
+
+class KeysList extends Component {
+
+
+    render() {
+
+        let listItems = []
+        for (let i = 0; i < 10000; i++) {
+            listItems.push({ id: i, content: i*i })
+        }
+
+
+        return (<ReactScrollableList
+                listItems={listItems}
+                heightOfItem={30}
+                maxItemsToRender={10}
+            />
+        )
+    }
+}
+
+export default KeysList;
