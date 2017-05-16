88b260fa067ce29b12706a26a3a753bdfba81ccc
diegowholikesorange <dieg@tognola.net>
2017-05-16 13:32:58 +1000

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,30 +1,108 @@
 import React, {Component} from "react";
-import "./App.css";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {fetchPostsIfNeeded, invalidateReddit, selectReddit} from "./actions";
+import Picker from "./components/Picker";
+import Posts from "./components/Posts";
 import RefreshButton from "./components/RefreshButton";
 import KeysList from "./components/KeysList";
-
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 
 class App extends Component {
+    static propTypes = {
+        selectedReddit: PropTypes.string.isRequired,
+        posts: PropTypes.array.isRequired,
+        isFetching: PropTypes.bool.isRequired,
+        lastUpdated: PropTypes.number,
+        dispatch: PropTypes.func.isRequired
+    }
+
+    componentDidMount() {
+        const {dispatch, selectedReddit} = this.props
+        dispatch(fetchPostsIfNeeded(selectedReddit))
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.selectedReddit !== this.props.selectedReddit) {
+            const {dispatch, selectedReddit} = nextProps
+            dispatch(fetchPostsIfNeeded(selectedReddit))
+        }
+    }
+
+    handleChange = nextReddit => {
+        this.props.dispatch(selectReddit(nextReddit))
+    }
+
+    handleRefreshClick = e => {
+        e.preventDefault()
+
+        const {dispatch, selectedReddit} = this.props
+        dispatch(invalidateReddit(selectedReddit))
+        dispatch(fetchPostsIfNeeded(selectedReddit))
+    }
 
     render() {
+        const {selectedReddit, posts, isFetching, lastUpdated} = this.props
+        const isEmpty = posts.length === 0
         return (
 
+
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
+                {/*<div className="App-header">*/}
+                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+                    {/*<h2>Welcome to React</h2>*/}
+                {/*</div>*/}
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <RefreshButton faIcon="plus" label="Add a new thing"/>
-                <KeysList/>
+                {/*<RefreshButton faIcon="plus" label="Add a new thing"/>*/}
+                {/*<KeysList/>*/}
+                <Picker value={selectedReddit}
+                        onChange={this.handleChange}
+                        options={['reactjs', 'frontend']}/>
+                <p>
+                    {lastUpdated &&
+                    <span>
+                        Last updated at {new Date(lastUpdated).toLocaleTimeString()}.
+                        {' '}
+                    </span>
+                    }
+                    {!isFetching &&
+                    <a href="#"
+                       onClick={this.handleRefreshClick}>
+                        Refresh
+                    </a>
+                    }
+                </p>
+                {isEmpty
+                    ? (isFetching ? <h2>Loading...</h2> : <h2>Empty.</h2>)
+                    : <div style={{opacity: isFetching ? 0.5 : 1}}>
+                        <Posts posts={posts}/>
+                    </div>
+                }
             </div>
-
-        );
+        )
     }
 }
 
-export default App;
+const mapStateToProps = state => {
+    const {selectedReddit, postsByReddit} = state
+    const {
+        isFetching,
+        lastUpdated,
+        items: posts
+    } = postsByReddit[selectedReddit] || {
+        isFetching: true,
+        items: []
+    }
+
+    return {
+        selectedReddit,
+        posts,
+        isFetching,
+        lastUpdated
+    }
+}
+
+export default connect(mapStateToProps)(App)
